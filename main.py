584b3ae96a72391367b833a5a90960fd66461f59
kjunice4 <37306150+kjunice4@users.noreply.github.com>
2023-10-16 13:50:46 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivmob import KivMob
+from kivy.app import App
+from kivy.uix.label import Label
+
+class BannerTest(App):
+    """ Displays a banner ad at top of the screen.
+    """
+
+    def build(self):
+        self.ads = KivMob('ca-app-pub-3940256099942544~3347511713')
+        self.ads.new_banner('ca-app-pub-3940256099942544/6300978111', top_pos=True)
+        self.ads.request_banner()
+        self.ads.show_banner()
+        return Label(text='Banner Ad Demo')
+
+if __name__ == "__main__":
+    BannerTest().run()
