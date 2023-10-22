9b0c0123bf1399c0e8660b6c9249570d4abe1528
kjunice4 <37306150+kjunice4@users.noreply.github.com>
2023-10-22 19:18:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from kivmob import KivMob
+from kivmob import KivMob, TestIds
 from kivy.app import App
 from kivy.uix.label import Label
 
@@ -7,8 +7,8 @@ class BannerTest(App):
     """
 
     def build(self):
-        self.ads = KivMob('ca-app-pub-3940256099942544~3347511713')
-        self.ads.new_banner('ca-app-pub-3940256099942544/6300978111', top_pos=True)
+        self.ads = KivMob(TestIds.APP)
+        self.ads.new_banner(TestIds.BANNER,True)
         self.ads.request_banner()
         self.ads.show_banner()
         return Label(text='Banner Ad Demo')
